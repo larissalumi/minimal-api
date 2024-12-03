8064534e1dd5c04a582efc180eb637744aac878b
Larissa Lumi <larissalumih@gmail.com>
2024-12-02 21:49:27 -0300

    atualização 1

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Infraestrutura.Db;
+using MinimalApi.DTOs; 
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options => {
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
